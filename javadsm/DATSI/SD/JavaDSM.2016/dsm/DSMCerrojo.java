2fd74b5b506884d4765315517a4f316390b53255
Samuel de Morais <sam.trasten@gmail.com>
2017-05-18 19:52:31 +0200

    Principio de DSMCerrojo

--- javadsm/DATSI/SD/JavaDSM.2016/dsm/DSMCerrojo.java
+++ javadsm/DATSI/SD/JavaDSM.2016/dsm/DSMCerrojo.java
@@ -5,11 +5,21 @@ import java.rmi.server.*;
 import java.util.*;
 
 public class DSMCerrojo {
+	
+	private List<ObjetoCompartido> listaDeObjetos = new ArrayList<ObjetoCompartido> ();
+	private String nombre;
+	private Almacen almacen;
 
     public DSMCerrojo (String nom) throws RemoteException {
+    	this.nombre = nom;
+    	String servidor = System.getenv("SERVIDOR");
+    	String puerto = System.getenv("PUERTO");
+    	this.almacen = (Almacen) Naming.lookup("rmi://" + servidor + ":" + puerto + "/Almacen");
+    	
     }
 
     public void asociar(ObjetoCompartido o) {
+    	listaDeObjetos.add(o);
     }
     public void desasociar(ObjetoCompartido o) {
     }
