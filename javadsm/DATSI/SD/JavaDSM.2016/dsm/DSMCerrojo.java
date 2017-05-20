02281d4312d42f618b2c9832ad20ee8c5a5d7b62
SAUL RODRIGUEZ TORRES <saul@localhost.localdomain>
2017-05-20 13:01:35 +0200

    DSMCerrojo.java: correccion del constructor

--- javadsm/DATSI/SD/JavaDSM.2016/dsm/DSMCerrojo.java
+++ javadsm/DATSI/SD/JavaDSM.2016/dsm/DSMCerrojo.java
@@ -4,18 +4,28 @@ import java.rmi.*;
 import java.rmi.server.*;
 import java.util.*;
 
+//public class DSMCerrojo implements Cerrojo{
+
 public class DSMCerrojo {
 	
-	private List<ObjetoCompartido> listaDeObjetos = new ArrayList<ObjetoCompartido> ();
-	private String nombre;
-	private Almacen almacen;
+    private List<ObjetoCompartido> listaDeObjetos = new ArrayList<ObjetoCompartido> ();
 
+    private String   nombre;        //nombre del cerrojo
+    private Almacen  almacen;       //almacen del cerrojo
+    private Cerrojo  cerrojo;       //el DSM utiliza este cerrojo por debajo.
+        
+    
     public DSMCerrojo (String nom) throws RemoteException {
     	this.nombre = nom;
     	String servidor = System.getenv("SERVIDOR");
     	String puerto = System.getenv("PUERTO");
-    	this.almacen = (Almacen) Naming.lookup("rmi://" + servidor + ":" + puerto + "/Almacen");
-    	
+	FabricaCerrojos fabrica = new FabricaCerrojos();
+    	this.almacen = (Almacen) Naming.lookup("rmi://" + servidor + ":" + puerto + "/DSM_almacen");
+	//DSM_almacen porque en el servidor:  Naming.rebind("rmi://localhost:" + args[0] + "/DSM_almacen", srv_alm);
+	fabrica = (FabricaCerrojos) Naming.lookup("rmi://" + servidor + ":" + puerto + "/DSM_cerrojos");
+	//DSM_cerrojos porque en el servidor: Naming.rebind("rmi://localhost:" + args[0] + "/DSM_cerrojos", srv_cerr);
+	
+	
     }
 
     public void asociar(ObjetoCompartido o) {
