a4118a8c1d88b37b27ad40cc115c866e1523c739
Saúl Rodríguez T <saulrodtorres@users.noreply.github.com>
2017-05-21 21:03:14 +0200

    Correcciones de compilación
    añadido el getInstance() para llamarlo desde el constructor

--- javadsm/DATSI/SD/JavaDSM.2016/dsm/FabricaCerrojosImpl.java
+++ javadsm/DATSI/SD/JavaDSM.2016/dsm/FabricaCerrojosImpl.java
@@ -2,26 +2,29 @@ package dsm;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.*;
-import java.util.HashMap;
 
 public class FabricaCerrojosImpl extends UnicastRemoteObject implements FabricaCerrojos {
-    static Map<String,Cerrojo> contenedorMap = new HashMap<>(); 
+    protected FabricaCerrojosImpl() throws RemoteException {
+		super();
+		getInstance();
+    }
+	static Map<String,Cerrojo> contenedorMap = new HashMap<>(); 
     static Cerrojo instancia = null;
     
-    public FabricaCerrojosImpl() throws RemoteException {
+    public static Cerrojo getInstance() throws RemoteException {
 	if (instancia == null){
-	    instancia = new Cerrojo();
+	    instancia = new CerrojoImpl();
 	}
 	return instancia;
     }
     public synchronized	Cerrojo iniciar(String s) throws RemoteException {
 	//creo que quiere que usemos la fabrica como singleton con contenedores
 	   //Cerrojo myreturn = null;
-	if (contenedorMap.contains(s)){
-	    return contenedorMap.get();
+	if (contenedorMap.containsKey(s)){
+	    return contenedorMap.get(s);
 	}
 	else{
-	    return FabricaCerrojosImpl();
+	    return getInstance();
 	}
     }
 }
